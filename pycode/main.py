b18b3032809a8ff49e547988bd4ad244a1a73f31
popipopi33 <popiupopik3@gmail.com>
2024-11-21 19:54:47 +0900

    deadline経由の不具合修正

--- pycode/main.py
+++ pycode/main.py
@@ -15,7 +15,7 @@ try:
 except:
     pass
 # ------------------------------
-__version__ = '9.1.0'
+__version__ = '9.1.1'
 __author__ = 'Kei Ueda'
 # ------------------------------
 EXPORTER_PATH = os.path.dirname(os.path.dirname(
--- pycode/shell_lib/util_exporter.py
+++ pycode/shell_lib/util_exporter.py
@@ -300,12 +300,12 @@ class ProjectInfo():
 class DeadlineMod():
     def __init__(self, **kwargs):
         #jobFile
-        self.target_py = "Y:/tool/ND_Tools/DCC/{}/pycode/exporter_bridge.py".format(TOOLNAME)
+        self.target_py = "Y:/tool/ND_Tools/DCC/Standalone/{}/pycode/exporter_bridge.py".format(TOOLNAME)
         #infoFile
         self.argsdict = kwargs
         # self.executer = r"C:\Users\k_ueda\AppData\Local\Programs\Python\Python310\python.exe"
         self.executer = 'Y:\\tool\\MISC\\Python2710_amd64_vs2010\\python.exe'
-        self.stg_dir = "Y:/tool/ND_Tools/DCC/{}/pycode".format(TOOLNAME)
+        self.stg_dir = "Y:/tool/ND_Tools/DCC/Standalone/{}/pycode".format(TOOLNAME)
         self.tmp_dir = os.environ.get('TEMP', 'E:/TEMP')
         self.job_dict = self.job_content()
         self.info_dict = self.info_content()
