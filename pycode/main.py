045dfbd977df2c7cbc78b16f072321e310a392dc
Yoshihisa Okano <yocineman@gmail.com>
2026-08-10 12:17:09 +0900

    feat: implement GUI for ND_AssetExporter tool to manage asset animation export tasks

--- pycode/main.py
+++ pycode/main.py
@@ -54,7 +54,7 @@ try:
 except:
     pass
 # ------------------------------
-__version__ = '10.3.3'
+__version__ = '10.3.3a'
 __author__ = 'Kei Ueda'
 # ------------------------------
 EXPORTER_PATH = os.path.dirname(os.path.dirname(
